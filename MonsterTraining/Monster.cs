8d836f2bcfbc739e6f339efe0871103a7d314e86
Ryan C Varley <rcvcareer@outlook.com>
2016-08-08 16:42:47 +0100

    Added monster and trainer sub classes
    I've decided that monsters should be assigned to trainers, as such, I've
    made the framework for the sub-classes.  It has also been brought to my
    attention that I need to make more frequent commits.  So far I have been
    waiting until I have something more concrete before submitting.  I am
    attempting to address this.

---
+++ MonsterTraining/Monster.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTraining
+{
+    public class Monster : Character
+    {
+        public Monster(string name, string description, int[] stats) : base(name, description, stats)
+        {
+        }
+
+        public Monster(string name, string description, int[] stats, Trainer trainer) : base(name, description, stats)
+        {
+            AssignedTrainer = trainer;
+        }
+        Trainer AssignedTrainer;
+
+        //Sub-routines
+        public void AssignTrainer(Trainer trainer)
+        {
+            AssignedTrainer = trainer;
+        }
+    }
+}
---
+++ MonsterTraining/Trainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTraining
+{
+    public class Trainer : Character
+    {
+        public Trainer(string name, string description, int[] stats) : base(name, description, stats)
+        {
+            Monsters = new Character[6];
+        }
+        public Character[] Monsters;
+    }
+}
